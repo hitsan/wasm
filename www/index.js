5f4a2c98a796529ae0302cb7ac8129ac73a068fe
hitsan <soledewa2828@gmail.com>
2021-11-07 10:49:25 +0000

    implement uploading a image

--- www/index.js
+++ www/index.js
@@ -1,26 +1,54 @@
 const js = import("./node_modules/@hitsan/wasm/wasm.js");
 
-const textbox1 = document.getElementById("num1")
-const textbox2 = document.getElementById("num2")
-const sendBtn = document.getElementById("send")
+// const textbox1 = document.getElementById("num1")
+// const textbox2 = document.getElementById("num2")
+// const sendBtn = document.getElementById("send")
 
-sendBtn.addEventListener('click', ()=>{
-  js.then(js => {
-    let msg;
-    let num1 = parseInt(textbox1.value, 10);
-    let num2 = parseInt(textbox2.value, 10);
-    console.log(num1);
-    msg = js.mul(num1, num2);
-    console.log(msg);
-    addElement(msg);
-  });
-})
+// sendBtn.addEventListener('click', ()=>{
+//   js.then(js => {
+//     let msg;
+//     let num1 = parseInt(textbox1.value, 10);
+//     let num2 = parseInt(textbox2.value, 10);
+//     console.log(num1);
+//     msg = js.mul(num1, num2);
+//     console.log(msg);
+//     addElement(msg);
+//   });
+// })
 
-const addElement = (msg)=>{
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode(msg);
-  newDiv.appendChild(newContent);
+// const addElement = (msg)=>{
+//   var newDiv = document.createElement("div");
+//   var newContent = document.createTextNode(msg);
+//   newDiv.appendChild(newContent);
 
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
+//   var currentDiv = document.getElementById("div1");
+//   document.body.insertBefore(newDiv, currentDiv);
+// }
+
+const selectImage = document.getElementById("select")
+
+const previewFile = (file) => {
+  // プレビュー画像を追加する要素
+  const preview = document.getElementById('preview');
+
+  // FileReaderオブジェクトを作成
+  const reader = new FileReader();
+
+  // ファイルが読み込まれたときに実行する
+  reader.onload = function (e) {
+    const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
+    const img = document.createElement("img"); // img要素を作成
+    img.src = imageUrl; // 画像のURLをimg要素にセット
+    preview.appendChild(img); // #previewの中に追加
+  }
+
+  // いざファイルを読み込む
+  reader.readAsDataURL(file);
 }
+
+const loadImage = () => {
+  const files = selectImage.files;
+  previewFile(files[0]);
+}
+
+selectImage.addEventListener('change', loadImage)
