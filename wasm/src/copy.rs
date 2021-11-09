d68a48ba36024c5c385b01f8d6791aa311b02de6
hitsan <soledewa2828@gmail.com>
2021-11-09 08:38:24 +0000

    fff

--- wasm/src/copy.rs
+++ wasm/src/copy.rs
@@ -3,6 +3,6 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn dump(height:u32, width:u32) -> u32{
+pub fn mul(height:u32, width:u32) -> u32{
     return height * width;
 }
--- www/index.js
+++ www/index.js
@@ -1,67 +1,35 @@
-const js = import("./node_modules/@hitsan/wasm/wasm.js");
-
-// const textbox1 = document.getElementById("num1")
-// const textbox2 = document.getElementById("num2")
-// const sendBtn = document.getElementById("send")
-
-// sendBtn.addEventListener('click', ()=>{
-//   js.then(js => {
-//     let msg;
-//     let num1 = parseInt(textbox1.value, 10);
-//     let num2 = parseInt(textbox2.value, 10);
-//     console.log(num1);
-//     msg = js.mul(num1, num2);
-//     console.log(msg);
-//     addElement(msg);
-//   });
-// })
-
-// const addElement = (msg)=>{
-//   var newDiv = document.createElement("div");
-//   var newContent = document.createTextNode(msg);
-//   newDiv.appendChild(newContent);
-
-//   var currentDiv = document.getElementById("div1");
-//   document.body.insertBefore(newDiv, currentDiv);
-// }
-
 const selectImage = document.getElementById("select")
 const changeImage = document.getElementById("change")
+const img = document.getElementById("previewImg");
 
 const previewFile = (file) => {
-  // プレビュー画像を追加する要素
   const preview = document.getElementById('preview');
-
-  // FileReaderオブジェクトを作成
   const reader = new FileReader();
 
-  // ファイルが読み込まれたときに実行する
   reader.onload = (e) => {
-    const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
-    const img = document.createElement("img"); // img要素を作成
+    const imageUrl = e.target.result;
+    // const img = document.createElement("img");
     img.id = "img"; 
-    img.src = imageUrl; // 画像のURLをimg要素にセット
-    preview.appendChild(img); // #previewの中に追加
-    image(imageUrl);
+    img.src = imageUrl;
+    preview.appendChild(img);
   }
 
-  // いざファイルを読み込む
   reader.readAsDataURL(file);
 }
 
-const image = () => {
-  const height = changeImage.naturalWidth ;
-  const width = changeImage.naturalHeight ;
-  const file = changeImage.src;
-  let ans;
+const image = async () => {
+  const result = await import("../wasm/pkg/wasm.js");
 
-  const resp = fetch(file);
-  const blob = resp.blob();
+  const canvas = document.getElementById("canvas");
+  canvas.width = img.naturalWidth;
+  canvas.height= img.naturalHeight;
 
-  js.then(js => {
-    ans = js.dump(height, width);
-  });
-  console.log(ans);
+  const ctx = canvas.getContext("2d");
+  ctx.textBaseline = 'middle';
+  ctx.textAlign = 'center';
+  ctx.font = '64px serif';
+
+  let ans = result.dump(height, width);
 }
 
 const loadImage = () => {
