d78661db69cf694750adf4f66c9b154f5e2c21e7
hitsan <soledewa2828@gmail.com>
2021-11-26 17:05:40 +0000

    get image data

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,23 +1,25 @@
 extern crate wasm_bindgen;
+extern crate image;
+extern crate imageproc;
 
 use wasm_bindgen::prelude::*;
+const WIDTH:u32 = 640;
+const HEIGHT:u32 = 330;
 
 #[wasm_bindgen]
 pub struct Share {
-    width: u32,
-    height: u32,
     pre: Vec<u8>,
     post: Vec<u8>,
 }
 
 #[wasm_bindgen]
 impl Share {
-    pub fn new(width: u32, height: u32) -> Share {
-        let pre: Vec<u8> = (0..(width*height*4))
+    pub fn new() -> Share {
+        let pre: Vec<u8> = (0..(WIDTH*HEIGHT*4))
             .map(|_| {0})
             .collect();
         let post = pre.clone();
-        Share {width, height, pre, post}
+        Share {pre, post}
     }
     pub fn get_pre(&self) -> *const u8 {
         self.pre.as_ptr()
@@ -25,14 +27,23 @@ impl Share {
     pub fn get_post(&self) -> *const u8 {
         self.post.as_ptr()
     }
-    pub fn rev(&mut self) {
-        let size = (self.width * self.height * 4) as usize;
-        for i in 0..size {
-            self.post[i] = self.pre[i];
-        }
+    pub fn rev(&mut self) -> Vec<u8> {
+        let arr = self.pre.clone();
+        let rgba_image = image::RgbaImage::from_vec(WIDTH, HEIGHT, arr);
+        let rgba_image = rgba_image.unwrap();
 
-        let mut later = self.post.split_off(size);
-        self.post.reverse();
-        self.post.append(&mut later);
+        imageproc::edges::canny()
+
+        let mut index = 0;
+        for h in 0..HEIGHT {
+            for w in 0..WIDTH {
+                let rgba = rgba_image.get_pixel(w, h);
+                for c in 0..4 {
+                    self.post[index] = rgba[c];
+                    index += 1;
+                }
+            }
+        }
+        return self.post.clone()
     }
 }
--- www/index.js
+++ www/index.js
@@ -6,24 +6,28 @@ const process = async () => {
   const wasm = await import('../wasm/pkg/wasm.js');
 
   const cvs = document.getElementById("preview");
-  const width = img.clientWidth;
-  const height = img.clientHeight;
+  const width = 640;
+  const height = 330;
+  img.width = width;
+  img.height = height;
   cvs.width = width;
   cvs.height = height;
   const ctx = cvs.getContext('2d');
 
-  let share = wasm.Share.new(width, height);
+  let share = wasm.Share.new();
 
   const mem = wasm_bg.memory;
   let pre_img = new Uint8ClampedArray(mem.buffer, share.get_pre() , width * height * 4);
-  let post_img = new Uint8ClampedArray(mem.buffer, share.get_post() , width * height * 4);
+  // let post_img = new Uint8ClampedArray(mem.buffer, share.get_post() , width * height * 4);
 
   ctx.drawImage(img, 0, 0, width, height);
   const img_pro = ctx.getImageData(0, 0, width, height);
 
   pre_img.set(img_pro.data);
-  share.rev();
-  const img2 = new ImageData(post_img, width);
+  let a = share.rev();
+  let image = Uint8ClampedArray(a);
+  console.log(a);
+  const img2 = new ImageData(image, width);
   ctx.putImageData(img2, 0, 0);
 }
 
